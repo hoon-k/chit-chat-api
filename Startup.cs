ff7501651e25e6ed52267dbcfcd8e2fa0e4ea44e
hoon-k <hoon.kong@pluralstack.io>
2019-05-21 10:22:29 -0700

    Add swagger middleware

--- Startup.cs
+++ Startup.cs
@@ -43,6 +43,10 @@ namespace ChitChatAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChitChat API V1");
+                });
             }
             else
             {
