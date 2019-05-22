3faeda22b9ab0b4b4ceb249b8b8bc97b526e738e
hoon-k <hoon.kong@pluralstack.io>
2019-05-22 11:50:16 -0700

    Re-name the API

--- src/user-api/Controllers/ValuesController.cs
+++ src/user-api/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ChitChatAPI.Controllers
+namespace UserAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
