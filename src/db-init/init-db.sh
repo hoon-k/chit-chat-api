96223deec054340fd5f6e2cc04f11be4a0f48909
hoon-k <hoon.kong@pluralstack.io>
2019-05-30 22:36:20 -0700

    discussion db

--- src/db-init/init-db.sh
+++ src/db-init/init-db.sh
@@ -4,5 +4,5 @@ psql -U postgres -c "CREATE DATABASE chitchat_users"
 psql -U postgres -c "CREATE DATABASE chitchat_reports"
 psql -U postgres -c "CREATE DATABASE chitchat_discussions"
 psql -U postgres -d chitchat_users -f /docker-entrypoint-initdb.d/init-chitchat-users.txt
+psql -U postgres -d chitchat_discussions -f /docker-entrypoint-initdb.d/init-chitchat-discussions.txt
 # psql -U postgres -d chitchat_users -f /docker-entrypoint-initdb.d/init-chitchat-reports.txt
-# psql -U postgres -d chitchat_users -f /docker-entrypoint-initdb.d/init-chitchat-discussions.txt
